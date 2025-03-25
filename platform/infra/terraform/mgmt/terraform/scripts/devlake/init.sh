d84299e80c8649e72a5298770bb0dbc96d3f52b0
Zach Jacobson <zjacobso@amazon.com>
2025-03-25 13:20:26 -0700

    rename

--- platform/infra/terraform/mgmt/terraform/scripts/devlake/init.sh
+++ platform/infra/terraform/mgmt/terraform/scripts/devlake/init.sh
@@ -4,7 +4,6 @@ kubectl wait --for=condition=ready pod -l devlakeComponent=lake -n devlake --tim
 kubectl wait --for=condition=ready pod -l devlakeComponent=mysql -n devlake --timeout=60s
 echo "Performing DB Migration"
 
-ADMIN_PASSWORD=$(kubectl get secret -n keycloak keycloak-config -o go-template='{{index .data "KEYCLOAK_ADMIN_PASSWORD" | base64decode}}')
 kubectl port-forward -n devlake svc/devlake-lake 9090:8080 >/dev/null 2>&1 &
 pid=$!
 trap '{
@@ -53,6 +52,11 @@ EOF
 )
 webhookCreateResponse=$(curl -X POST localhost:9090/plugins/webhook/connections -d "$webhookCreateRequest")
 webhookID=$(echo $webhookCreateResponse | jq -r '.id')
+webhookApiKey=$(echo $webhookCreateResponse | jq -r '.apiKey.apiKey')
+postIssuesEndpoint=$(echo $webhookCreateResponse | jq -r '.postIssuesEndpoint')
+postPullRequestsEndpoint=$(echo $webhookCreateResponse | jq -r '.postPullRequestsEndpoint')
+postPipelineDeployTaskEndpoint=$(echo $webhookCreateResponse | jq -r 'postPipelineDeployTaskEndpoint')
+
 # {"name":"modengg_gitea","id":1,"createdAt":"2025-03-21T23:47:42.486Z","updatedAt":"2025-03-21T23:47:42.486Z","postIssuesEndpoint":"/rest/plugins/webhook/connections/1/issues","closeIssuesEndpoint":"/rest/plugins/webhook/connections/1/issue/:issueKey/close","postPullRequestsEndpoint":"/rest/plugins/webhook/connections/1/pull_requests","postPipelineTaskEndpoint":"/rest/plugins/webhook/connections/1/cicd_tasks","postPipelineDeployTaskEndpoint":"/rest/plugins/webhook/connections/1/deployments","closePipelineEndpoint":"/rest/plugins/webhook/connections/1/cicd_pipeline/:pipelineName/finish","apiKey":{"id":1,"createdAt":"2025-03-21T23:47:42.490396162Z","updatedAt":"2025-03-21T23:47:42.490396162Z","creator":"admin","creatorEmail":"","updater":"admin","updaterEmail":"","name":"webhook-1","apiKey":"fPaKeWtWhTUdtlpneoibhrtvwiMbBiPL3022Yyda30WrqrrBTX882NQ1b5uhLK4kzwQCMi4Qxl65m2Tfvc9h12nwR1btEmB2wbkMARbg0FQNTxAwFudqN9u7QB1atuLT","expiredAt":null,"allowedPath":"/plugins/webhook/connections/1/.*","type":"plugin:webhook","extra":"connectionId:1"}}
 
 blueprintPatchRequest=$(
