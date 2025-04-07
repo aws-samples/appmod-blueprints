4691ca37851be52eb2ea732d8d333560daa96cff
Zach Jacobson <zjacobso@amazon.com>
2025-04-07 13:54:14 -0700

    fix subnets and yq
    fix yq
    
    fix for testing
    
    fixes
    
    fix policy
    
    fix policy again
    
    fix perms
    
    fix jobs
    
    fix job
    
    fix job
    
    fix values
    
    fix external
    
    some fixes for backstage
    
    test
    
    fixes

--- platform/infra/terraform/mgmt/terraform/mgmt-cluster/vpc.tf
+++ platform/infra/terraform/mgmt/terraform/mgmt-cluster/vpc.tf
@@ -7,8 +7,8 @@ module "vpc" {
 
   azs             = local.azs
   private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
-  database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 8)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+  database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k + 4)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k + 8)]
 
   enable_nat_gateway = true
   single_nat_gateway = true
