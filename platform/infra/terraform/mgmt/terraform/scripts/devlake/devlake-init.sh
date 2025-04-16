237a698a7bfc96b195bde9d3b98cd08f3f0b3552
Zach Jacobson <zjacobso@amazon.com>
2025-04-16 14:05:22 -0700

    change to crossplane xrd with environment config

--- platform/infra/terraform/mgmt/terraform/scripts/devlake/devlake-init.sh
+++ platform/infra/terraform/mgmt/terraform/scripts/devlake/devlake-init.sh
@@ -17,21 +17,25 @@ done
 
 
 OS=$(uname -s)
-STRINGS=("dotnet" "go" "java" "nextjs" "")
+STRINGS=("dotnet" "go" "java" "nextjs")
 for str in "${STRINGS[@]}"; do
-./project-init.sh $str
-  process_args "$MAC_SCRIPT" "$str"
+  key_conn_bp=$(./project-init.sh $str)
+  key=$(echo key_conn_bp | cut -d'|' -f1)
+  conn=$(echo key_conn_bp | cut -d'|' -f2)
+  bp=$(echo key_conn_bp | cut -d'|' -f3)
+  case "$OS" in
+    "Darwin")
+      ./generate-data-mac.sh http://localhost:9090 $key $conn
+      ;;
+    "Linux")
+      ./generate-data-linux.sh http://localhost:9090 $key $conn
+      ;;
+    *)
+      echo "Unsupported operating system: $OS"
+      exit 1
+      ;;
+  esac
+  curl -X POST localhost:9090/blueprints/$bp/trigger
+
 done
 
-case "$OS" in
-  "Darwin")
-    ./generate-data-mac.sh
-    ;;
-  "Linux")
-    ./generate-data-linux.sh
-    ;;
-  *)
-    echo "Unsupported operating system: $OS"
-    exit 1
-    ;;
-esac
