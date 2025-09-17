03147e30cc58e9d3d25052de1915e5b75b6ea82a
Sébastien Allamand <sallaman@amazon.com>
2025-09-17 11:03:06 +0200

    fix baskstage compile 1
    Signed-off-by: Sébastien Allamand <sallaman@amazon.com>

--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -7,7 +7,7 @@ backend.add(import('@backstage/plugin-app-backend'));
 backend.add(import('@backstage/plugin-proxy-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
-backend.add(import('@backstage/plugin-scaffolder-backend-module-gitlab'));
+backend.add(import('@internal/plugin-scaffolder-backend-module-gitlab'));
 backend.add(import('@backstage/plugin-techdocs-backend'));
 
 // roadiehq plugins
