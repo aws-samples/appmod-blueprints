d3da610651b861dc147b203983fcf3652f5dd17c
Pankaj Walke <punkwalker@gmail.com>
2025-10-09 01:02:10 +0000

    make gitlab_repository_setup idempotent
    Signed-off-by: Pankaj Walke <punkwalker@gmail.com>

--- platform/infra/terraform/scripts/utils.sh
+++ platform/infra/terraform/scripts/utils.sh
@@ -293,19 +293,14 @@ gitlab_repository_setup(){
   if ! git diff --quiet || ! git diff --cached --quiet; then
     git add .
     git commit -m "Updated bootstrap values in Backstag template and Created spoke cluster secret files " || true
+    if ! git push --set-upstream gitlab HEAD:main --force-with-lease; then
+      if ! git push gitlab HEAD:main --force-with-lease; then
+        log_error "Failed to push repository to GitLab"
+        exit 1
+      fi
+    fi
   else
     print_info "No changes to commit"
-  fi
-
-  if ! git pull --rebase gitlab main; then
-    log_warning "Failed to pull with rebase from GitLab"
-  fi
-  
-  if ! git push --set-upstream gitlab HEAD:main --force; then
-    if ! git push gitlab HEAD:main --force; then
-      log_error "Failed to push repository to GitLab"
-      exit 1
-    fi
   fi
 
   cd -
