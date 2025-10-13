aa26d138a18f7fc5b656e14c793bf510f411f0a1
Pankaj Walke <punkwalker@gmail.com>
2025-10-13 22:06:09 +0000

    add usage telemetry
    Signed-off-by: Pankaj Walke <punkwalker@gmail.com>

--- platform/infra/terraform/common/variables.tf
+++ platform/infra/terraform/common/variables.tf
@@ -1,3 +1,13 @@
+################################################################################
+# Usage Telemetry
+################################################################################
+
+variable "usage_tracking_tag" {
+  description = "Tag to identify Modern Engineering on AWS usage within tracking tools"
+  type        = string
+  default     = "uksb-u7h6yeonth"
+}
+
 variable "resource_prefix" {
   description = "Prefix for project"
   type        = string
