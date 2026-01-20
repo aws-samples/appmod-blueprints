7c1c5b449458c2e5b80ed5f8cf3744835edb53ef
Workshop User <workshopuser@example.com>
2026-01-20 14:22:21 +0000

    add argo var
    Signed-off-by: Workshop User <workshopuser@example.com>

--- platform/infra/terraform/common/deploy.sh
+++ platform/infra/terraform/common/deploy.sh
@@ -144,6 +144,7 @@ main() {
 
   # Get ArgoCD domain from Terraform output
   export ARGOCD_DOMAIN=$(terraform -chdir=$DEPLOY_SCRIPTDIR output -raw ingress_domain_name)
+  export INGRESS_DOMAIN=$ARGOCD_DOMAIN
 
   # Update backstage default values now that both domains are available
   update_backstage_defaults
